1ae59948efee980fb5cac74af3cdd1aec2f0c608
mujang <mujangabe@gmail.com>
2020-03-06 14:07:07 -0600

    man

--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,46 @@
-from flask import render_template
+from flask import render_template,request
 from app import app
-
-
+#app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html",title="Home")
-
-
 @app.route("/aboutus")
 def aboutus():
     return render_template("aboutus.html",title="About Us",)
-
 @app.route("/frequentlyaskquestions")
 def frequentlyaskquestions():
-    return render_template("frequentlyaskquestions.html",title="Frequently Ask Question")
-
+    return render_template("frequentlyaskquestions.html",title="Frequently Ask Questions")
 @app.route("/routes")
 def routes():
-    return "routes"
-
-@app.route("/login")
+    return render_template("routes.html",title="Routes")
+@app.route("/login",methods=['GET','POST'])
 def login():
-    return "login"
+   # post_username=str(request.form['uname'])
+    #post_password=str(request.form['psw'])
+    return render_template("login.html",title="Login") 
+
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+    usertype={"1":"shipper","2":"traveller","3":"middleman"}
+    if request.method == "POST":
+        post_email = str(request.form["email"])
+        post_password= str(request.form["password"])
+        #usertype=str(request.form["usertype"] )
+        print(post_email,post_password)
+        return redirect(url_for("home"))
+    else:
+        return render_template("register.html", usertype=usertype,title="Register")
+
+
+@app.route("/traveler",methods=['GET','POST'])
+def traveler():
+    return render_template("traveler.html",title="Traveler")
+@app.route("/shipper",methods=['GET','POST'])
+def shipper():
+    return render_template("shipper.html",title="Shipper")
+@app.route("/middleman",methods=['GET','POST'])
+def middleman():
+   return render_template("middleman.html",title="Middleman")
+
+
